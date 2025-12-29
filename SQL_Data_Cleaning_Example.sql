4e229e39c70322babab898ded06cd0306bc62ad0
Emertonian <iemerton@gmail.com>
2025-12-29 20:12:23 +0000

    Update SQL_Data_Cleaning_Example.sql

--- SQL_Data_Cleaning_Example.sql
+++ SQL_Data_Cleaning_Example.sql
@@ -1,8 +1,7 @@
 -- SQL Project - Data Cleaning
 
+-- Dataset found at...
 -- https://www.kaggle.com/datasets/swaptr/layoffs-2022
-
-
 
 
 
@@ -12,13 +11,15 @@ FROM world_layoffs.layoffs;
 
 
 
--- first thing we want to do is create a staging table. This is the one we will work in and clean the data. We want a table with the raw data in case something happens
+-- first thing I created a staging table as I wanted to keep a table containing the raw data in case something happens.
+
 CREATE TABLE world_layoffs.layoffs_staging 
 LIKE world_layoffs.layoffs;
 
 INSERT layoffs_staging 
 SELECT * FROM world_layoffs.layoffs;
 
+-- I will perform the following steps...
 
 -- now when we are data cleaning we usually follow a few steps
 -- 1. check for duplicates and remove any
